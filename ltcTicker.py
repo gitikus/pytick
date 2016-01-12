4d1b4152b37705bbd7043cbabf44509a8716d821
pc <pc@ubuntu.com>
2016-01-12 16:41:10 +1100

    simple ltc price updates

---
+++ ltcTicker.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python 2.7
+import time ,json
+import requests
+import sys
+
+def btce_ltc_usd():
+    btceBtcTick = requests.get('https://btc-e.com/api/3/ticker/ltc_usd')
+    return btceBtcTick.json()['ltc_usd']['last']
+
+def huobi():
+    huobiBook = requests.get('http://api.huobi.com/staticmarket/detail_ltc_json.js')
+    return huobiBook.json()['p_new']
+
+def okcoinCNY():
+    okCNYlast = requests.get('https://www.okcoin.cn/api/v1/ticker.do?symbol=ltc_cny')
+    return okCNYlast.json()['ticker']['last']
+
+def bitfinex():
+    finexLast = requests.get('https://api.bitfinex.com/v1/ticker/ltcusd')
+    return finexLast.json()['last_price']
+
+
+
+
+if __name__ == '__main__':
+    while True:
+        huobiLTC = huobi()
+        print 'huobi:',huobiLTC
+        ok = okcoinCNY()
+        print 'okcoin:',ok
+        btce = btce_ltc_usd()
+        print 'btce:', btce
+        bfinex = bitfinex()
+        print 'finex:',bfinex
+        print '----------'
+        time.sleep(2.8)
+            #2.5 hits huobi limi
